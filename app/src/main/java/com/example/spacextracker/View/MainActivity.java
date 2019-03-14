5e0536017aa6d7fe2c0efc3992016b119ca9eeba
Lundevs <louis7513@yahoo.fr>
2019-03-14 10:50:45 +0100

    Added loading animation

--- app/src/main/java/com/example/spacextracker/View/MainActivity.java
+++ app/src/main/java/com/example/spacextracker/View/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.spacextracker.View;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +9,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
