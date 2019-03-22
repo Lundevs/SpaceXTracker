601c38b46d0f94a768b3a93b8440eb4f85e96d14
lundev <louis7513@yahoo.fr>
2019-03-22 11:31:14 +0100

    added StatsController

--- app/src/main/java/com/example/spacextracker/View/StatsActivity.java
+++ app/src/main/java/com/example/spacextracker/View/StatsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.spacextracker.Controller.StatsController;
 import com.example.spacextracker.Model.Launches;
 import com.example.spacextracker.R;
 
@@ -13,10 +14,17 @@ public class StatsActivity extends AppCompatActivity {
 
     List<Launches> dataLaunches;
 
+    StatsController controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        controller = new StatsController(this);
+
+    }
+
+    public void showData(List<Launches> launchList){
 
     }
 }
